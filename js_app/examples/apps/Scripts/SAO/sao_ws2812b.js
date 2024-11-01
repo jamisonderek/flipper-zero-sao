2a7ce8e99b594814c914bdc2b36375e4a3d890bb
Derek Jamison <jamisonderek@users.noreply.github.com>
2024-11-01 14:07:28 -0600

    Update for Blinky Loop SAO pins.

--- js_app/examples/apps/Scripts/SAO/sao_ws2812b.js
+++ js_app/examples/apps/Scripts/SAO/sao_ws2812b.js
@@ -3,19 +3,29 @@ let ws2812 = require('ws2812');
 
 let max = 255 / 6;
 
-ws2812.setup({ pin: 2, count: 7 });
+ws2812.setup({ pin: 2, count: 12 });
 
+for (let i=0; i<100000;i++) {
 // does not update the LEDs when called, only updates the internal buffer
-ws2812.set(0, { red: 0, green: max, blue: max });
-ws2812.set(1, { red: 0, green: max, blue: 0 });
-ws2812.set(2, { red: 0, green: 0, blue: max });
-ws2812.set(3, { red: max, green: max, blue: 0 });
-ws2812.set(4, { red: max, green: 0, blue: max });
-ws2812.set(5, { red: max, green: 0, blue: 0 });
-ws2812.set(6, { red: max / 2, green: max / 2, blue: 0 });
+ws2812.set(i%12, { red: 0, green: max, blue: max });
+ws2812.set((i+1)%12, { red: 0, green: max, blue: 0 });
+ws2812.set((i+2)%12, { red: 0, green: 0, blue: max });
+ws2812.set((i+3)%12, { red: max, green: max, blue: 0 });
+ws2812.set((i+4)%12, { red: max, green: 0, blue: max });
+ws2812.set((i+5)%12, { red: max, green: 0, blue: 0 });
+ws2812.set((i+6)%12, { red: max / 2, green: max / 2, blue: 0 });
+ws2812.set((i+7)%12, { red: 0, green: max, blue: max });
+ws2812.set((i+8)%12, { red: 0, green: max, blue: 0 });
+ws2812.set((i+9)%12, { red: 0, green: 0, blue: max });
+ws2812.set((i+10)%12, { red: max, green: max, blue: 0 });
+ws2812.set((i+11)%12, { red: max, green: 0, blue: max });
 
 // send the data to the LEDs
 ws2812.update();
+
+delay(70);
+
+}
 
 let color = ws2812.getColor(6);
 print("Color[6]", color.red, color.green, color.blue);
